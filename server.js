307a83bdcb2ecafd9ccaef34e616e648f1643ab7
Joey Vinyard <jviny98@yahoo.com>
2017-12-26 23:23:54 -0500

    Cleaner code

--- server.js
+++ server.js
@@ -10,26 +10,30 @@ var options = {
 		reconnect: true
 	},
 	identity: {
-		username: "smolbitbot",
+		username: config.username,
 		password: config.oAuth
 	},
-	channels: ["nanopierogi"]
+	channels: [config.channel]
 }
 
 var client = new tmi.client(options);
 client.connect();
 
-// client.on("connected", function(address, port){
-// 	client.say("nanopierogi", "I am here! Ping me!");
-// })
-// client.on("chat", function(channel, user, message, self){
-// 	parseMessage(channel, user, message);
-// })
+client.on("connected", function(address, port){
+	// client.say("nanopierogi", "I am here! Ping me!");
+})
+client.on("chat", function(channel, user, message, self){
+	if(self)//Ignore if the bot sent this message
+		return;
+	parseMessage(channel, user, message);
+})
 
-// function parseMessage(channel, user, message){
-// 	if(user.username == "smolbitbot")
-// 		return;
-// 	if(message == "ping"){
-// 		client.say("nanopierogi", "pong");
-// 	}
-// }
+function parseMessage(channel, user, message){
+	if(message.charAt(0) == '!'){
+		var args = message.substring(1).toLowerCase().split(" ");
+		var command = args[0];
+		if(command=="ping"){
+			client.say(config.channel, "pong");
+		}
+	}
+}
