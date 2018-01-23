e770af54ce3a020e223a8c9b813d31d56dc7814a
JoeyVinyard <jviny98@yahoo.com>
2018-01-23 13:38:41 -0500

    Dashboard work

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -23,6 +24,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     RouterModule.forRoot(ROUTES)
   ],
--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+	config = {
+		addReg: "Viewer",
+		command: "Viewer",
+		edit: "Viewer"
+	}
+	permOptions = ["Viewer", "Regular", "Subscriber", "Moderator", "Broadcaster"];
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
