a2a3841548e06dbb09a733083b6b3fa1b9e63c4a
Joey Vinyard <jviny98@yahoo.com>
2018-05-07 22:38:20 -0400

    Few backend fixes

--- server/web.js
+++ server/web.js
@@ -4,13 +4,20 @@ var url = require('url');
 var twitch = require('./twitchAPI');
 
 http.createServer(function (req, res) {
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 	var request = url.parse(req.url, true);
 	if(request.pathname == '/oauth'){
+		if(!request.query.token){
+			console.log("No token provided");
+			res.statusCode = 400;
+			res.write("{}");
+			res.end();
+			return;
+		}
 		twitch.getOAuthToken(request.query.token).then((data) => {
 			var token = {
 				token: data.data.access_token
 			}
-			res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
 			res.statusCode = 200;
 			res.write(JSON.stringify(token));
 			res.end();
